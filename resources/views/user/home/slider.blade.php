b525cfcd8e2216e081bac713e85280a835bc1407
ManhPM <phammanhbeo2001@gmail.com>
2024-10-04 00:36:00 +0700

    xử lý thiếu phần search khi merge, chuẩn bị triển khai refactor lại file css

--- resources/views/user/home/slider.blade.php
+++ resources/views/user/home/slider.blade.php
@@ -1,73 +1,75 @@
-<div class="row container container-fluid p-0">
-    <div id="slide-show" class="container-fluid col-10 p-0">
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner wrap-slide">
-                <div class="carousel-item active">
-                    <div class="d-flex justify-content-center align-items-center position-relative">
-                        <div class="row">
-                            <div class="content col-5">
-                                <h2>Phiên bản</h2>
-                                <h3>Pro Apple Magic Keyboard mỏng nhất</h3>
-                                <p>Floating cantilever design. </p>
-                                <p><button type="button" class="btn">Mua ngay</button></p>
-                            </div>
-                            <div
-                                class="image-box d-flex justify-content-center align-items-center position-relative  col-7">
-                                <img src="{{ asset('public/user/assets/images/img-slider-01-home2.png') }}" class="d-block " alt="First Slide">
-                            </div>
+<div class="row container-fluid container p-0">
+				<div id="slide-show" class="container-fluid col-10 p-0">
+								<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+												<div class="carousel-inner wrap-slide">
+																<div class="carousel-item active">
+																				<div class="d-flex justify-content-center align-items-center position-relative">
+																								<div class="row">
+																												<div class="content col-5">
+																																<h2>Phiên bản</h2>
+																																<h3>Pro Apple Magic Keyboard mỏng nhất</h3>
+																																<p>Floating cantilever design. </p>
+																																<p><button type="button" class="btn">Mua ngay</button></p>
+																												</div>
+																												<div
+																																class="image-box d-flex justify-content-center align-items-center position-relative col-7">
+																																<img src="{{ asset('public/user/assets/images/img-slider-01-home2.png') }}"
+																																				class="d-block" alt="First Slide">
+																												</div>
 
-                        </div>
+																								</div>
 
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="d-flex justify-content-center align-items-center position-relative">
-                        <div class="row">
-                            <div class="content col-5 ">
-                                <h2>Ghế xịn Velvet</h2>
-                                <h3>bọc nhung Easyfashion</h3>
-                                <p>Option color: Pink or Aqua. </p>
-                                <p><button type="button" class="btn">Mua ngay</button></p>
-                            </div>
-                            <div
-                                class="image-box d-flex justify-content-center align-items-center position-relative  col-7">
-                                <img src="{{ asset('public/user/assets/images/img-slider-02-home2.png') }}" class="d-block col-7"
-                                    alt="First Slide">
-                            </div>
-                        </div>
+																				</div>
+																</div>
+																<div class="carousel-item">
+																				<div class="d-flex justify-content-center align-items-center position-relative">
+																								<div class="row">
+																												<div class="content col-5">
+																																<h2>Ghế xịn Velvet</h2>
+																																<h3>bọc nhung Easyfashion</h3>
+																																<p>Option color: Pink or Aqua. </p>
+																																<p><button type="button" class="btn">Mua ngay</button></p>
+																												</div>
+																												<div
+																																class="image-box d-flex justify-content-center align-items-center position-relative col-7">
+																																<img src="{{ asset('public/user/assets/images/img-slider-02-home2.png') }}"
+																																				class="d-block col-7" alt="First Slide">
+																												</div>
+																								</div>
 
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="d-flex justify-content-center align-items-center position-relative">
-                        <div class="row">
-                            <div class="content col-5">
-                                <h2>Phiên bản</h2>
-                                <h3>Pro Apple Magic Keyboard mỏng nhất</h3>
-                                <p>Floating cantilever design. </p>
-                                <p><button type="button" class="btn">Mua ngay</button></p>
-                            </div>
-                            <div
-                                class="image-box d-flex justify-content-center align-items-center position-relative  col-7">
-                                <img src="{{ asset('public/user/assets/images/img-slider-03-home2.png') }}" class="d-block col-7"
-                                    alt="First Slide">
-                            </div>
-                        </div>
+																				</div>
+																</div>
+																<div class="carousel-item">
+																				<div class="d-flex justify-content-center align-items-center position-relative">
+																								<div class="row">
+																												<div class="content col-5">
+																																<h2>Phiên bản</h2>
+																																<h3>Pro Apple Magic Keyboard mỏng nhất</h3>
+																																<p>Floating cantilever design. </p>
+																																<p><button type="button" class="btn">Mua ngay</button></p>
+																												</div>
+																												<div
+																																class="image-box d-flex justify-content-center align-items-center position-relative col-7">
+																																<img src="{{ asset('public/user/assets/images/img-slider-03-home2.png') }}"
+																																				class="d-block col-7" alt="First Slide">
+																												</div>
+																								</div>
 
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev slider-button-left" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <i class="fa fa-chevron-left" aria-hidden="true"></i>
-            </button>
-            <button class="carousel-control-next slider-button-right" type="button" data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next">
-                <i class="fa fa-chevron-right" aria-hidden="true"></i>
-            </button>
-        </div>
-        <div class="box-image">
-            <img src="{{ asset('public/user/assets/images/banner-home2-01.jpg') }}" alt=""><img src="{{ asset('public/user/assets/images/banner-home2-02222.jpg') }}"
-                alt="">
-        </div>
-    </div>
+																				</div>
+																</div>
+												</div>
+												<button class="carousel-control-prev slider-button-left" type="button"
+																data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+																<i class="fa fa-chevron-left" aria-hidden="true"></i>
+												</button>
+												<button class="carousel-control-next slider-button-right" type="button"
+																data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+																<i class="fa fa-chevron-right" aria-hidden="true"></i>
+												</button>
+								</div>
+								<div class="box-image">
+												<img src="{{ asset('public/user/assets/images/banner-home2-01.jpg') }}" alt=""><img
+																src="{{ asset('public/user/assets/images/banner-home2-02222.jpg') }}" alt="">
+								</div>
+				</div>
 </div>
