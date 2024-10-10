ed844508bd5a46e011f077f5f2b74a86cb48ec1e
ManhPM <phammanhbeo2001@gmail.com>
2024-10-10 13:54:04 +0700

    fix modal logout appear blur block after submit

--- public/admin/assets/js/setup.js
+++ public/admin/assets/js/setup.js
@@ -353,7 +353,6 @@ function endAjax(element, text){
 
 $(document).ready(function () {
     $("form").submit(function(){
-		$(this).prepend('<div style="position: absolute;width: 100%;height: 100%;background: #ffffff91;z-index: 10;"></div>')
         $(this).find("button[type='submit']").css("opacity", "0.5");
         $(this).find("button[type='submit']").html('<span class="spinner-grow spinner-grow-sm"></span> Đang xử lý..');
     });
